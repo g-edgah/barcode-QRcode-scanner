c2011ffc3341c13e5c91e1e6b3f08733794e9546
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-09-28 14:14:21 +0300

    scanner code

---
+++ index.js
@@ -0,0 +1,18 @@
+function onScanSuccess(decodedText, decodedResult) {
+    // succesfull scan handling
+    document.querySelector('result').innerHTML= `${decodedText}`;
+    console.log(`Code matched = ${decodedText}`, decodedResult);
+}
+
+function onScanFailure (error) {
+    // failed scan handling
+    document.querySelector('result').innerHTML = `error: ${error}`;
+    console.warn(`Code scan error = ${error}`);
+}
+
+let html5QrcodeScanner = new Html5QrcodeScanner(
+    "reader",
+    {fps: 10, qrbox: {width:250, height: 250}},
+    false
+);
+html5QrcodeScanner.render(onScanSuccess, onScanFailure);
